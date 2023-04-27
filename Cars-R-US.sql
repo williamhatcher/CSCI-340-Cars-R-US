6d59027d9e2b11ce62c2b75de6baee590f8837fc
William Hatcher <william@hatcher.work>
2023-04-27 11:41:10 -0500

    Fix broken Vehicle_Packages

--- Cars-R-US.sql
+++ Cars-R-US.sql
@@ -1,66 +1,67 @@
 DROP DATABASE IF EXISTS Dealership;
 CREATE DATABASE Dealership;
+USE Dealership;
 
 CREATE TABLE `Vehicles` (
-  `vin` varchar(20) PRIMARY KEY,
-  `make` varchar(20) NOT NULL,
-  `model` varchar(20) NOT NULL,
-  `exterior_color` varchar(20) NOT NULL,
-  `interior_color` varchar(20) NOT NULL,
-  `model_year` year NOT NULL,
-  `drivetrain` varchar(20) NOT NULL,
-  `is_new` bool NOT NULL
+                            `vin` varchar(20) PRIMARY KEY,
+                            `make` varchar(20) NOT NULL,
+                            `model` varchar(20) NOT NULL,
+                            `exterior_color` varchar(20) NOT NULL,
+                            `interior_color` varchar(20) NOT NULL,
+                            `model_year` year NOT NULL,
+                            `drivetrain` varchar(20) NOT NULL,
+                            `is_new` bool NOT NULL
 );
 
 CREATE TABLE `Inventory` (
-  `stock_id` int PRIMARY KEY AUTO_INCREMENT,
-  `vin` varchar(20) NOT NULL,
-  `landed_cost` decimal(10,2) NOT NULL,
-  `asking_price` decimal(10,2) NOT NULL,
-  `incoming_mileage` int NOT NULL,
-  `sale_id` int COMMENT 'If not present, vehicle can be sold'
+                             `stock_id` int PRIMARY KEY AUTO_INCREMENT,
+                             `vin` varchar(20) NOT NULL,
+                             `landed_cost` decimal(10,2) NOT NULL,
+                             `asking_price` decimal(10,2) NOT NULL,
+                             `incoming_mileage` int NOT NULL,
+                             `sale_id` int COMMENT 'If not present, vehicle can be sold'
 );
 
 CREATE TABLE `Salesperson` (
-  `sp_id` varchar(20) PRIMARY KEY,
-  `name` varchar(20) NOT NULL,
-  `hire_year` year NOT NULL,
-  `commission_rate` int NOT NULL
+                               `sp_id` varchar(20) PRIMARY KEY,
+                               `name` varchar(20) NOT NULL,
+                               `hire_year` year NOT NULL,
+                               `commission_rate` int NOT NULL
 );
 
 CREATE TABLE `Sales` (
-  `sale_id` int AUTO_INCREMENT,
-  `stock_id` int NOT NULL,
-  `customer_id` int NOT NULL,
-  `sp_id` varchar(20) NOT NULL,
-  `sold_price` decimal(10,2) NOT NULL,
-  PRIMARY KEY (`sale_id`, `stock_id`)
+                         `sale_id` int AUTO_INCREMENT,
+                         `stock_id` int NOT NULL,
+                         `customer_id` int NOT NULL,
+                         `sp_id` varchar(20) NOT NULL,
+                         `sold_price` decimal(10,2) NOT NULL,
+                         PRIMARY KEY (`sale_id`, `stock_id`)
 );
 
 CREATE TABLE `Customers` (
-  `customer_id` int PRIMARY KEY,
-  `name` varchar(20) NOT NULL,
-  `phone` varchar(12) NOT NULL,
-  `email` varchar(20) NOT NULL,
-  `birthday` date NOT NULL
+                             `customer_id` int PRIMARY KEY,
+                             `name` varchar(20) NOT NULL,
+                             `phone` varchar(12) NOT NULL,
+                             `email` varchar(20) NOT NULL,
+                             `birthday` date NOT NULL
 );
 
 CREATE TABLE `VehicleAttributes` (
-  `vin` varchar(20) NOT NULL,
-  `attribute` varchar(20) NOT NULL,
-  `value` varchar(20) NOT NULL,
-  PRIMARY KEY (`vin`, `attribute`)
+                                     `vin` varchar(20) NOT NULL,
+                                     `attribute` varchar(20) NOT NULL,
+                                     `value` varchar(20) NOT NULL,
+                                     PRIMARY KEY (`vin`, `attribute`)
 );
 
 CREATE TABLE `Packages` (
-  `pkg_id` int PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL,
-  `contents` text NOT NULL
+                            `pkg_id` int PRIMARY KEY AUTO_INCREMENT,
+                            `name` varchar(20) NOT NULL,
+                            `contents` text NOT NULL
 );
 
 CREATE TABLE `Vehicle_Packages` (
-  `vin` int PRIMARY KEY,
-  `package_id` varchar(20) NOT NULL
+                                    `vin` varchar(20) PRIMARY KEY,
+                                    `package_id` int NOT NULL
 );
 
 CREATE INDEX `VehicleAttributes_index_0` ON `VehicleAttributes` (`attribute`);
